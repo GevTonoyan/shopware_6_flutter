231c80aba5c1218a975164f1bd0144953c3bd9db
Gevorg <yourgitemail@example.com>
2025-03-14 18:30:47 +0400

    Fix video for android

--- lib/feature_products/presentation/ui/product_card.dart
+++ lib/feature_products/presentation/ui/product_card.dart
@@ -25,7 +25,8 @@ class ProductCard extends StatelessWidget {
           SizedBox(
             height: 300,
             width: 200,
-            child: ProductVideoWebView(videoUrl: product.imageUrl),
+            //child: ProductVideoWebView(videoUrl: product.imageUrl),
+            child: VideoWebView(videoUrl: product.imageUrl),
           ),
           const SizedBox(height: 24),
           Text(product.name, style: typography.subtitleBold),
@@ -43,28 +44,53 @@ class ProductCard extends StatelessWidget {
   }
 }
 
-class ProductVideoWebView extends StatefulWidget {
+class VideoWebView extends StatefulWidget {
   final String videoUrl;
 
-  const ProductVideoWebView({Key? key, required this.videoUrl}) : super(key: key);
+  const VideoWebView({super.key, required this.videoUrl});
 
   @override
-  _ProductVideoWebViewState createState() => _ProductVideoWebViewState();
+  State<VideoWebView> createState() => _VideoWebViewState();
 }
 
-class _ProductVideoWebViewState extends State<ProductVideoWebView> {
+class _VideoWebViewState extends State<VideoWebView> {
   late InAppWebViewController webViewController;
 
   @override
   Widget build(BuildContext context) {
+    String htmlContent = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <style>
+        body { margin: 0; padding: 0; overflow: hidden; background: black; }
+        video {
+          width: 100vw; height: 100vh;
+          object-fit: cover;
+          pointer-events: none; /* Disable any touch interactions */
+        }
+      </style>
+    </head>
+    <body>
+      <video id="video" autoplay loop muted playsinline>
+        <source src="${widget.videoUrl}" type="video/webm">
+        Your browser does not support the video tag.
+      </video>
+      <script>
+        document.addEventListener("DOMContentLoaded", function() {
+          var video = document.getElementById("video");
+          video.removeAttribute("controls"); // Ensure controls are hidden
+          video.play();
+        });
+      </script>
+    </body>
+    </html>
+    """;
+
     return Scaffold(
       body: InAppWebView(
-        initialUrlRequest: URLRequest(url: WebUri.uri(Uri.parse(widget.videoUrl))),
-        initialSettings: InAppWebViewSettings(
-          javaScriptEnabled: true,
-          mediaPlaybackRequiresUserGesture: false,
-          mediaType: 'webm',
-        ),
+        initialData: InAppWebViewInitialData(data: htmlContent),
         onWebViewCreated: (controller) {
           webViewController = controller;
         },
